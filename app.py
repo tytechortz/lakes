e24403e231c0b8810a93de8e236427b9901f7261
James Swank <jmswank7@gmail.com>
2019-11-11 07:56:45 -0700

    creates revered df

--- app.py
+++ app.py
@@ -9,22 +9,37 @@ import time
 
 today = time.strftime("%Y-%m-%d")
 
-df_lp = pd.read_csv('https://water.usbr.gov/api/web/app.php/api/series?sites=lakepowell&parameters=Day.Inst.ReservoirStorage.af&start=1850-01-01&end='+ today +'&format=csv', skiprows=4)
-
+df_powell = pd.read_csv('https://water.usbr.gov/api/web/app.php/api/series?sites=lakepowell&parameters=Day.Inst.ReservoirStorage.af&start=1850-01-01&end='+ today +'&format=csv', skiprows=4)
+df_powell['Date'] = pd.to_datetime(df_powell['Date'])
+df_powell.set_index(['Date'], inplace=True)
+print(df_powell)
+df_lp = df_powell.iloc[::-1]
 print(df_lp)
+# df_lp['Date'] = pd.to_datetime(df_lp['Date'])
 
+# print(df_lp)
+levels = df_lp['Value']
+# print(levels)
+# print(df_lp.iloc[1000,3])
+# print(df_lp['Value'])
 def get_layout():
     return html.Div(
         [
-            dcc.Graph(
-                data = go.Scatter(
-                y = df_lp['Value'],
-                x = df_lp['Date'],
-                ),
-                layout = go.Layout(
-                height = 500
+            html.Div([
+                dcc.Graph(
+                    id='lake-levels',
                 )
-            )
+            ],
+                className='row'
+            ),
+            html.Div([
+                dcc.Dropdown(
+                    id='lake',
+                    options=[
+                        {'label': 'Lake Powell', 'value': 'lp'}
+                    ]
+                )
+            ])
         ]
     )
 
@@ -32,19 +47,21 @@ app = dash.Dash(__name__)
 app.layout = get_layout
 app.config['suppress_callback_exceptions']=True
 
-# @app.callback(
-#     Output('graph', 'figure'),
-#     [Input('')
-# )
-# def lakepowell_graph():
-#     data = go.Scatter(
-#         y = df_lp['Value'],
-#         x = df_lp['Date'],
-#     )
-#     layout = go.Layout(
-#         height = 500
-#     )
-#     return {'data': data, 'layout': layout}
+@app.callback(
+    Output('lake-levels', 'figure'),
+    [Input('lake', 'value')])
+def lakepowell_graph(lake):
+    print(lake)
+    trace = [
+        go.Scatter(
+            y = df_lp['Value'],
+            # x = df_lp['Date'],
+        ),
+    ],
+    layout = go.Layout(
+        height = 500
+    )
+    return {'data': trace, 'layout': layout}
 
 if __name__ == "__main__":
     app.run_server(port=8020, debug=False)
