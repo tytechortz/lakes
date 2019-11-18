4cef4e78933adeb6d5d6726e67473ebbb3c692ea
James Swank <jmswank7@gmail.com>
2019-11-18 09:16:00 -0700

    creates lake mead table

--- create_db.py
+++ create_db.py
@@ -5,12 +5,12 @@ import time
 
 today = time.strftime("%Y-%m-%d")
 
-df = pd.read_csv('https://water.usbr.gov/api/web/app.php/api/series?sites=lakepowell&parameters=Day.Inst.ReservoirStorage.af&start=1850-01-01&end='+ today +'&format=csv', skiprows=4)
+df = pd.read_csv('https://water.usbr.gov/api/web/app.php/api/series?sites=hdmlc&parameters=Day.Inst.ReservoirStorage.af&start=1850-01-01&end='+ today +'&format=csv', skiprows=4)
 
 engine = sqlalchemy.create_engine("postgresql://postgres:1234@localhost/lakes")
 con = engine.connect()
 
-table_name = 'lake_powell'
+table_name = 'lake_mead'
 df.to_sql(table_name, con)
 print(engine.table_names())
 
